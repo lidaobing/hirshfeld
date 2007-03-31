679f3c5962feca87b4a7c135b8aaee2ce67f8a5c
lidaobing <lidaobing>
2007-03-31 04:19:56 +0000

    add long-options-support

---
+++ lib/version_etc_copyright.c
@@ -0,0 +1,10 @@
+#include <config.h>
+
+/* Specification.  */
+#include "version-etc.h"
+
+const char version_etc_copyright[] =
+  /* Do *not* mark this string for translation.  %s is a copyright
+     symbol suitable for this locale, and %d is the copyright
+     year.  */
+  "Copyright %s %d LI Daobing <lidaobing@gmail.com>";
